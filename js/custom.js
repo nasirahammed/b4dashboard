d0781683f1a93105d3d02d2600da4f0fa2828d06
nasirahammed <developer.nasir@outlook.com>
2018-12-24 01:35:02 +0530

    Final Commit

---
+++ js/custom.js
@@ -0,0 +1,110 @@
+$(document).ready(function() {
+  // Toggle Sidebar, Main Content, Footer on cick of Navigation Menu 
+$(".nav-click").click(function() {
+  $(".main-sidebar").toggleClass("sidebar-canvas");
+  $(".main-content").toggleClass("main-canvas");
+  $(".main-footer").toggleClass("footer-canvas");
+});
+
+// Highchart for Line 
+
+Highcharts.chart('lineContainer', {
+  chart: {
+      type: 'line'
+  },
+  title: {
+      text: 'Monthly Average Temperature'
+  },
+  subtitle: {
+      text: 'Source: WorldClimate.com'
+  },
+  xAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  },
+  yAxis: {
+      title: {
+          text: 'Temperature (°C)'
+      }
+  },
+  plotOptions: {
+      line: {
+          dataLabels: {
+              enabled: true
+          },
+          enableMouseTracking: false
+      }
+  },
+  series: [{
+      name: 'Tokyo',
+      data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+  }, {
+      name: 'London',
+      data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+  }]
+});
+
+// Highchart for Stack
+
+
+Highcharts.chart('stackContainer', {
+  chart: {
+      type: 'column'
+  },
+  title: {
+      text: 'Stacked column chart'
+  },
+  xAxis: {
+      categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+  },
+  yAxis: {
+      min: 0,
+      title: {
+          text: 'Total fruit consumption'
+      },
+      stackLabels: {
+          enabled: true,
+          style: {
+              fontWeight: 'bold',
+              color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+          }
+      }
+  },
+  legend: {
+      align: 'right',
+      x: -30,
+      verticalAlign: 'top',
+      y: 25,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+      borderColor: '#CCC',
+      borderWidth: 1,
+      shadow: false
+  },
+  tooltip: {
+      headerFormat: '<b>{point.x}</b><br/>',
+      pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+  },
+  plotOptions: {
+      column: {
+          stacking: 'normal',
+          dataLabels: {
+              enabled: true,
+              color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+          }
+      }
+  },
+  series: [{
+      name: 'John',
+      data: [5, 3, 4, 7, 2]
+  }, {
+      name: 'Jane',
+      data: [2, 2, 3, 2, 1]
+  }, {
+      name: 'Joe',
+      data: [3, 4, 4, 2, 5]
+  }]
+});
+
+
+});
+
